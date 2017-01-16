65902c1fd520a476340d25b9982eeae0ba325af8
bbiggins <brian@Brian.local>
2017-01-16 08:41:15 -0600

    Chat board styling
    Font changes, chat display height

--- rrs-client/src/components/chat/chat-board/ChatBoard.vue
+++ rrs-client/src/components/chat/chat-board/ChatBoard.vue
@@ -1,5 +1,7 @@
 <template>
 <div id="chatBoard">
+  <center class="headers">Activity Feed</center>
+  <div class="line"></div>
   <chat-display :task_id.sync="taskId"></chat-display>
   <chat-enter :task_id.sync="taskId"></chat-enter>
 </div>
